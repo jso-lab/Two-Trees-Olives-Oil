e8de0ee0cb852645b64690a131404e89c5da214d
Jean Paul <jpaulcamillo@outlook.fr>
2022-05-08 00:19:46 +0200

    make it better

--- config.php
+++ config.php
@@ -1,15 +1,15 @@
 <?php
     function db() {
         $connexion = null;
-        $host = '	clwxydcjair55xn0.chr7pe7iynqr.eu-west-1.rds.amazonaws.com';
-        $db_name = 'ivk1vpns5qho1kcg';
-        $username = 'p7037fxtkvft3hzf';
-        $password = 'u07yic6hx8ud3ujt';
+        $host = 'clwxydcjair55xn0.chr7pe7iynqr.eu-west-1.rds.amazonaws.com';
+        $db_name = 'ikeqeo8swliybja6';
+        $username = 'a84h6arzwszj1xuo';
+        $password = 'bhtbonpcp0lz0j60';
         try{
             $connexion = new PDO("mysql:host=" . $host . ";dbname=" . $db_name, $username, $password);
             $connexion->exec("set names utf8");
         }catch(PDOException $exception){
-            echo "Connection error: " . $exception->getMessage();
+            echo "Impossible de se connecter. Erreur: " . $exception->getMessage();
         }
 
         return $connexion;
