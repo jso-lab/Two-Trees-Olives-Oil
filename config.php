c91697f54cf1dc81f9ab2076391414b1b8f7b1cf
Jean Paul <jpaulcamillo@outlook.fr>
2022-05-08 00:12:10 +0200

    make it better

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
     function db() {
         $connexion = null;
-        $host = 'localhost';
-        $db_name = 'ecomerce';
-        $username = 'root';
-        $password = '';
+        $host = '	clwxydcjair55xn0.chr7pe7iynqr.eu-west-1.rds.amazonaws.com';
+        $db_name = 'ivk1vpns5qho1kcg';
+        $username = 'p7037fxtkvft3hzf';
+        $password = 'u07yic6hx8ud3ujt';
         try{
             $connexion = new PDO("mysql:host=" . $host . ";dbname=" . $db_name, $username, $password);
             $connexion->exec("set names utf8");
